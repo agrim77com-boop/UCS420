1d2c6378573ed0475553d24660305b0fab6b1160
Agrim_47 <agrim77.com@gmail.com>
2026-08-04 08:38:22 +0530

    Create max_of_3num.py

---
+++ max_of_3num.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter first number: "))
+c = int(input("Enter first number: "))
+if(a>b>c):
+  print(a,"is the max")
+if(b>a>c):
+  print(b,"is the max")
+else :
+  print(c,"is the max")
